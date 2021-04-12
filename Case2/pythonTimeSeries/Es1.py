fd4e498a6f7b950da4acdd3eb3fef9b64b24c9aa
paoloceravolo <paolo.ceravolo@unimi.it>
2021-04-12 11:40:28 +0200

    Update Es1.py

--- Case2/pythonTimeSeries/Es1.py
+++ Case2/pythonTimeSeries/Es1.py
@@ -1,3 +1,63 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import seaborn as sns
 
 df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'], index_col='date')
+
+#print(df)
+
+## Visualize entire series 
+# Draw Plot
+def plot_df(df, x, y, title="", xlabel='Date', ylabel='Value', dpi=100):
+    plt.figure(figsize=(16,5), dpi=dpi)
+    plt.plot(x, y, color='tab:red')
+    plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
+    #plt.show()
+
+plot_df(df, x=df.index, y=df.value, title='Monthly anti-diabetic drug sales in Australia from 1992 to 2008.')    
+
+df.reset_index(inplace=True)
+# extract year and mont from the data value
+df['year'] = [d.year for d in df.date]
+df['month'] = [d.strftime('%b') for d in df.date]
+years = df['year'].unique()
+
+# Prep Colors
+#np.random.seed(100)
+mycolors = np.random.choice(list(mpl.colors.XKCD_COLORS.keys()), len(years), replace=False)
+
+# Draw Plot
+plt.figure(figsize=(16,12), dpi= 80)
+for i, y in enumerate(years):
+    if i > 0:        
+        plt.plot('month', 'value', data=df.loc[df.year==y, :], color=mycolors[i], label=y)
+        plt.text(df.loc[df.year==y, :].shape[0]-.9, df.loc[df.year==y, 'value'][-1:].values[0], y, fontsize=12, color=mycolors[i])
+
+# Decoration
+plt.gca().set(xlim=(-0.3, 11), ylim=(2, 30), ylabel='$Drug Sales$', xlabel='$Month$')
+plt.yticks(fontsize=12, alpha=.7)
+plt.title("Seasonal Plot of Drug Sales Time Series", fontsize=20)
+#plt.show()
+
+## Visualize by boxplot 
+
+# Draw Plot
+fig, axes = plt.subplots(1, 2, figsize=(20,7), dpi= 80)
+sns.boxplot(x='year', y='value', data=df, ax=axes[0])
+sns.boxplot(x='month', y='value', data=df.loc[~df.year.isin([1991, 2008]), :])
+
+# Set Title
+axes[0].set_title('Year-wise Box Plot\n(The Trend)', fontsize=18); 
+axes[1].set_title('Month-wise Box Plot\n(The Seasonality)', fontsize=18)
+plt.show()
+
+
+
+
+
+
+
+
+
