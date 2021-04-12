4a559ced115d42084bc5bcc97094ca12a87bc23f
paoloceravolo <paolo.ceravolo@unimi.it>
2021-04-12 13:17:15 +0200

    Uploading Es2.py

--- Case2/pythonTimeSeries/Es1.py
+++ Case2/pythonTimeSeries/Es1.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import seaborn as sns
+import antropy as ant # https://github.com/raphaelvallat/antropy
+
 
 from statsmodels.tsa.seasonal import seasonal_decompose
 
@@ -76,7 +78,103 @@ result_add = seasonal_decompose(df['value'], model='additive', extrapolate_trend
 plt.rcParams.update({'figure.figsize': (10,10)})
 result_mul.plot().suptitle('Multiplicative Decompose', fontsize=22)
 result_add.plot().suptitle('Additive Decompose', fontsize=22)
-plt.show()
+#plt.show()
+
+# Actual Values = Product of (Seasonal * Trend * Resid)
+df_reconstructed = pd.concat([result_mul.seasonal, result_mul.trend, result_mul.resid, result_mul.observed], axis=1)
+df_reconstructed.columns = ['seas', 'trend', 'resid', 'actual_values']
+print(df)
+print(df_reconstructed)
+
+# Using statmodels: Subtracting the Trend Component.
+
+detrended = df.value.values - result_mul.trend
+plt.plot(detrended)
+plt.title('Drug Sales detrended by subtracting the trend component', fontsize=16)
+#plt.show()
+
+# Permutaiton Entropy
+print('Permutaiton Entropy', ant.perm_entropy(df['value'], normalize=True))
+
+# Autocorrelation
+
+print('Autocorrelation with lag 2', df['value'].autocorr(lag=2))
+print('Autocorrelation with lag 4', df['value'].autocorr(lag=4))
+print('Autocorrelation with lag 8', df['value'].autocorr(lag=8))
+print('Autocorrelation with lag 16', df['value'].autocorr(lag=16))
+print('Autocorrelation with lag 32', df['value'].autocorr(lag=32))
+
+from statsmodels.graphics import tsaplots
+
+# Display the autocorrelation plot of your time series
+fig = tsaplots.plot_acf(df['value'], lags=24)
+#plt.show()
+
+## Discretize the time series
+#https://github.com/seninp/saxpy
+# SAX
+
+from saxpy.znorm import znorm
+from saxpy.sax import ts_to_string
+from saxpy.alphabet import cuts_for_asize
+
+#print(ts_to_string(znorm(df['value']), cuts_for_asize(5)))
+
+# SAX conversion via sliding window
+# the result is represented as a data structure of resulting words and their respective positions on time series:
+
+from saxpy.hotsax import find_discords_hotsax
+from saxpy.paa import paa
+from saxpy.sax import sax_via_window
+sax_win = sax_via_window(df['value'], win_size=6, paa_size=6, alphabet_size=3, nr_strategy=None, z_threshold=0.01)
+#print(sax_win)
+#print([x for x in sax_win]) #list of sequences
+
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+
+model = ARIMA(df.value, order=(4,2,3))
+model_fit = model.fit(disp=0)
+#print(model_fit.summary())
+#print(model_fit.forecast(1))
+
+# Create Training and Test
+train = df.value[:180]
+test = df.value[180:]
+
+fc, se, conf = model_fit.forecast(24, alpha=0.05) 
+
+#print(fc)
+
+# Make as pandas series
+fc_series = pd.Series(fc, index=test.index)
+lower_series = pd.Series(conf[:, 0], index=test.index)
+upper_series = pd.Series(conf[:, 1], index=test.index)
+
+plt.figure(figsize=(12,5), dpi=100)
+plt.plot(train, label='training')
+plt.plot(test, label='actual')
+plt.plot(fc_series, label='ARIMA forecast')
+plt.fill_between(lower_series.index, lower_series, upper_series, 
+                 color='k', alpha=.15)
+plt.title('Forecast vs Actuals')
+plt.legend(loc='upper left', fontsize=8)
+#plt.show()
+
+from sklearn.metrics import mean_absolute_error
+
+mae_ar = mean_absolute_error(test, fc_series)
+print('ARIMA MAE: %.3f' % mae_ar)
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Case2/pythonTimeSeries/Es2.py
@@ -0,0 +1,89 @@
+# forecast with random forest
+from numpy import asarray
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import concat
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
+from matplotlib import pyplot
+
+# Transform a time series dataset into a supervised learning dataset
+# We can restructure this time series dataset as a supervised learning problem 
+# by using the value at the previous time step to predict the value at the next time-step.
+# This means that methods that randomize the dataset during evaluation, like k-fold cross-validation, cannot be used.
+# The function below will take a time series as a NumPy array time series with one or more columns and transform it into 
+# a supervised learning problem with the specified number of inputs and outputs.
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = DataFrame(data)
+	cols = list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+	# put it all together
+	agg = concat(cols, axis=1)
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg.values
+
+# split a univariate dataset into train/test sets, n_test is the number of observations to be used for splitting
+def train_test_split(data, n_test):
+	return data[:-n_test, :], data[-n_test:, :]
+
+# fit an random forest model and make a one step prediction
+def random_forest_forecast(train, testX):
+	# transform list into array
+	train = asarray(train)
+	# split into input and output columns
+	trainX, trainy = train[:, :-1], train[:, -1]
+	# fit model
+	model = RandomForestRegressor(n_estimators=1000)
+	model.fit(trainX, trainy)
+	# make a one-step prediction
+	yhat = model.predict([testX])
+	#print('yhat', yhat[0])
+	return yhat[0]
+
+# walk-forward validation for univariate data
+# In walk-forward validation, the dataset is first split into train and test sets by selecting a cut point with n_test
+def walk_forward_validation(data, n_test):
+	predictions = list()
+	# split dataset
+	train, test = train_test_split(data, n_test)
+	# seed history with training dataset
+	history = [x for x in train]
+	# step over each time-step in the test set
+	for i in range(len(test)):
+		# split test row into input and output columns
+		testX, testy = test[i, :-1], test[i, -1]
+		# fit model on history and make a prediction
+		yhat = random_forest_forecast(history, testX)
+		# store forecast in list of predictions
+		predictions.append(yhat)
+		# add actual observation to history for the next loop
+		history.append(test[i])
+		# summarize progress in the shell
+		# print('>expected=%.1f, predicted=%.1f' % (testy, yhat))
+	# estimate prediction error
+	error = mean_absolute_error(test[:, -1], predictions)
+	return error, test[:, -1], predictions
+
+# load the dataset
+series = read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', header=0, index_col=0)
+values = series.values
+# transform the time series data into supervised learning
+# incresing n_in, the number of observation to be taken in input to the RF you get better predction
+data = series_to_supervised(values, n_in=1)
+# evaluate
+# firs parameter is the dataset, the second parameter is the cut point (n_test), i.e. the number of observations used in the training stage
+mae, y, yhat = walk_forward_validation(data, 150)
+print('MAE: %.3f' % mae)
+# plot expected vs predicted
+pyplot.plot(y, label='Expected')
+pyplot.plot(yhat, label='Predicted')
+pyplot.legend()
+pyplot.show()
