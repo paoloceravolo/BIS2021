a2956a8f7f20d4543d96ca2f263d1e19f3066eb9
paoloceravolo <paolo.ceravolo@unimi.it>
2021-04-19 13:10:14 +0200

    Case 4 sequences

--- Case4/python/Es1.py
+++ Case4/python/Es1.py
@@ -6,33 +6,66 @@ from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, fpmax, fpgrowth
 from mlxtend.frequent_patterns import association_rules
 
+from pm4py.objects.log.util import dataframe_utils
+from pm4py.objects.conversion.log import converter as log_converter
+from pm4py.algo.filtering.log.variants import variants_filter
 
-df = pd.read_csv('./saleslog.csv', parse_dates=['Date'])
+df1 = pd.read_csv('./saleslog.csv', parse_dates=['Date'])
+df2 = pd.read_csv('./wine_description.csv')
 
-print(df)
+df_merge = pd.merge(df1,df2, left_on='itemDescription', right_on='name')
 
-itemlist = df.groupby('Member_number')['itemDescription'].apply(list)\
-.reset_index().rename(columns={'itemDescription':'Selected Items'})
+df_merge['Origin_Type'] = df_merge.apply(lambda row: row.origin +'-'+ row.type, axis=1)
 
-print(itemlist)
+print(df_merge)
 
-matrix = [itemlist['Selected Items'][i] for i,v in enumerate(itemlist['Selected Items'])]
+log_csv = dataframe_utils.convert_timestamp_columns_in_df(df_merge)
+log_csv = log_csv.sort_values('Date') # sort by the timestamp column
+# inverting itemDescription and type we can make the trace abstraction lower or higher
+log_csv.rename(columns={'Member_number': 'case:concept:name', 'Date': 'time:timestamp', 'Origin_Type': 'concept:name', 'origin': 'org:resource'}, inplace=True) #change the name to a colum
+parameters = {log_converter.Variants.TO_EVENT_LOG.value.Parameters.CASE_ID_KEY: 'case:concept:name'} # identify the case_id_key name (if not changed it will simply be the mane of the coloumn)
+event_log = log_converter.apply(log_csv, parameters=parameters, variant=log_converter.Variants.TO_EVENT_LOG)
 
-print(matrix)
+## Count solt products
 
-te = TransactionEncoder()
-te_ary = te.fit(matrix).transform(matrix)
-df2 = pd.DataFrame(te_ary, columns=te.columns_)
+count_product_type = log_csv.groupby('org:resource').agg(\
+Type=('org:resource', 'count'),\
+# Multiple aggregations of the same column using pandas ...
+FirstOccurence=('time:timestamp', lambda x: x.min()),
+LastOccurence=('time:timestamp', lambda x: x.max()),
+Endurance=('time:timestamp', lambda x: x.max() - x.min()),
+)
+print(count_product_type)
 
-print(df2)
+count_product = log_csv.groupby('concept:name').agg({\
+'concept:name': 'count'})
 
-frequent_itemsets = fpgrowth(df2, min_support=0.1, use_colnames=True)
+print(count_product)
 
-print(frequent_itemsets)
+## List variants 
+variants = variants_filter.get_variants(event_log)
+# number of events, cases, and variants included in the list
+print('Events:', len(log_csv), '- Cases: ', len(event_log),'- Variants:', len(variants))
 
-rules = association_rules(frequent_itemsets, metric='lift', min_threshold=0.1)
+#print(variants)
 
-print(rules)
+## Directly-Follows Graph
+
+from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
+from pm4py.visualization.dfg import visualizer as dfg_visualization
+
+# In principle other variants and parameters are available but they are not suported in the last version of PM4ML 
+# https://pm4py.fit.fraunhofer.de/static/assets/api/1.3.4/_modules/pm4py/algo/discovery/dfg/algorithm.html 
+dfg = dfg_discovery.apply(event_log, variant=dfg_discovery.Variants.FREQUENCY)
+# this setting of the parameters is required to save the result is an SVG file
+parameters = {dfg_visualization.Variants.FREQUENCY.value.Parameters.FORMAT: "svg"}
+gviz = dfg_visualization.apply(dfg, log=event_log, variant=dfg_visualization.Variants.FREQUENCY, parameters=parameters)
+
+#dfg_visualization.view(gviz)
+dfg_visualization.save(gviz, "dfg.svg")
+
+
+
 
 
 
